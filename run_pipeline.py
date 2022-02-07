b030824f2d7dc6fa0dc1153367df4d396d93178c
AidanJones4 <ajones@spartaglobal.com>
2022-02-07 18:49:47 +0000

    Data cleaning for Talent csv's implemented

--- run_pipeline.py
+++ run_pipeline.py
@@ -13,18 +13,19 @@ print(json_pip.dataframe.head())
 
 print("\n\ncsv Talent Dataframe:")
 csv_talent_pip = Pipeline("data-26-final-project-files", "Talent", "csv", "candidate_data.json")
-csv_talent_pip.extract()
+csv_talent_pip.extract(force=True)
 print(csv_talent_pip.dataframe.head())
 
 print("\n\ncsv Academy Dataframe:")
 csv_academy_pip = Pipeline("data-26-final-project-files", "Academy", "csv", "academy_data.json")
-csv_academy_pip.extract()
+csv_academy_pip.extract(force=True)
 print(csv_academy_pip.dataframe.head())
 
 print("\n\ntxt Dataframe:")
 txt_pip = Pipeline("data-26-final-project-files", "Talent", "txt", "sparta_day_data.json")
-txt_pip.extract()
+txt_pip.extract(force=True)
 print(txt_pip.dataframe.head())
+
 
 
 
